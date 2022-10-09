da78abfad96e79f4d435cb055c6737baa0cd0b10
Manoj Basnet <basnetm02@gmail.com>
2022-10-09 20:53:57 +0545

    First Commit

---
+++ src/components/OperationButton.jsx
@@ -0,0 +1,9 @@
+import {ACTION} from '../App';
+
+export default function OperationButton({dispatch,operation}){
+    return(
+        <button onClick={() => dispatch({type:ACTION.CHOOSE_OPERATION,payload:{operation}})}>
+            {operation}
+            </button>
+    )
+}
